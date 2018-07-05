868ea1ba066050b77003c5ac48eaa3b67a4f22a7
b1acksun <tkachenko.igor88@gmail.com>
2018-07-05 09:51:02 +0300

    fix tests

--- test/methods.test.js
+++ test/methods.test.js
@@ -2,17 +2,14 @@ import assert from 'assert'
 import golos from '../src'
 
 import { camelCase } from '../src/utils'
-import { methods_0_16_4 } from './methods_by_version'
+import { methods_0_18_0 } from './methods_by_version'
 
 describe('golos.methods', () => {
 
     it('has all generated methods', () => {
 
-        const methods = methods_0_16_4
+        const methods = methods_0_18_0
             .map( m => `${camelCase(m)}`)
-            .concat([
-                'getMarketOrderBook'
-            ])
             .sort()
 
         let libMethods = Object.keys(golos.api.Golos.prototype)
